1edceb71310e7cc9ebef5d843638da65afd5259e
Udrasht <110713655+Udrasht@users.noreply.github.com>
2022-08-11 18:44:54 +0530

    Update q2.sh

--- q2.sh
+++ q2.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 touch extra.txt
-chmod +x extra.txt
-cat /etc/shells > extra.txt
-while read p
-do
-awk '$p=="usr" { FS = "/" } ; { print $4 }' extra.txt
-done
-#echo $p
+#chmod +x extra.txt
+cat /etc/shells >> extra.txt
+
+awk ' BEGIN{ FS="/" };
+{if ($2=="usr") print $4}' extra.txt
 
